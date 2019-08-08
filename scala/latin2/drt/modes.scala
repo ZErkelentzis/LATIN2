0db8d40bd5c076b8b988f65d37aa4e037436fc5a
Florian Rabe <florian.rabe@gmail.com>
2019-08-08 16:18:42 +0300

    no message

--- scala/latin2/drt/modes.scala
+++ scala/latin2/drt/modes.scala
@@ -13,15 +13,17 @@ import lf._
 
 object PUnion extends BinaryLFConstantScala(DRT._path, DRT.punion.name)
 object Empty extends NullaryLFConstantScala(DRT._path, DRT.empty.name)
+object Close extends UnaryLFConstantScala(DRT._path, DRT.close.name)
 
 object NormalizePunion extends ComputationRule(DRT.punion.path) with ApplicableUnder {
+  override def alternativeHeads = List(DRT.close.path)
   def under = List(Apply.path)
   def apply(check: CheckingCallback)(tm: Term, covered: Boolean)(implicit stack: Stack, history: History): Simplifiability = {
-    tm match {
-      case PUnion(_) =>
+    val (args, closed) = tm match {
+      case PUnion(_) => (PUnion.associativeArguments(tm).distinct, false)
+      case Close(m) => (PUnion.associativeArguments(m).distinct, true)
       case _ => return Simplifiability.NoRecurse
     }
-    val args = PUnion.associativeArguments(tm).distinct
     val args2 = Empty.filter(args)
     var pos: List[Term] = Nil
     var neg: List[Term] = Nil
@@ -33,10 +35,19 @@ object NormalizePunion extends ComputationRule(DRT.punion.path) with ApplicableU
     }}
     neg = neg diff pos
     val (otherPos,otherArgs) = other.unzip
+    if (closed && other.isEmpty)
+      pos = Nil
     val argsS = pos.reverseMap(i => DRT.pos(i)) ::: neg.reverseMap(i => DRT.neg(i)) ::: otherArgs.reverse
-    val tS = PUnion.assoc(Empty.term, argsS)
-    if (tm != tS)
-      Simplify(tS)
+    val tUnion = PUnion.assoc(Empty.term, argsS)
+    val tClosed = if (closed) {
+      if (other.isEmpty)
+        tUnion
+      else
+        Close(tUnion)
+    } else
+      tUnion
+    if (tm != tClosed)
+      Simplify(tClosed)
     else
       RecurseOnly(otherPos)
   }
