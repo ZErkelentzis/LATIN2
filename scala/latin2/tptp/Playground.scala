372b8852aa2eafe5ebaf6547bce6575e00ec67fe
XBagon <xbagon@outlook.de>
2021-03-09 06:55:50 +0100

    implement SFOLExporter

--- scala/latin2/tptp/Playground.scala
+++ scala/latin2/tptp/Playground.scala
@@ -1,24 +1,12 @@
 package latin2.tptp
 
-import info.kwarc.mmt.api.{LocalName, Path}
-import info.kwarc.mmt.api.frontend.{Controller, Extension}
-import info.kwarc.mmt.api.modules.Theory
+import info.kwarc.mmt.api.frontend.Extension
 import info.kwarc.mmt.api.objects._
-import info.kwarc.mmt.api.symbols.Constant
-import info.kwarc.mmt.api.uom.SimplificationUnit
-import info.kwarc.mmt.lf.{ApplySpine, Lambda}
+import info.kwarc.mmt.api.{LocalName, Path}
 import latin2.sfol.CommonSymbols
-import latin2.sfol.CommonSymbols.DedList
-import leo.datastructures.TPTP._
 import lf.Conjunction.and
-import lf.Disjunction.or
-import lf.Equivalence.equiv
-import lf.ExistentialQuantification.exists
-import lf.Implication.impl
-import lf.Negation.not
 import lf.Proofs.ded
 import lf.Propositions.prop
-import lf.UniversalQuantification.forall
 
 object Playground extends Extension {
   override def logPrefix: String = "tptp-playground"
@@ -32,133 +20,19 @@ object Playground extends Extension {
     log("yes, it does: " + controller.getConstant(CommonSymbols.prop.path).toString)
 
     //OLD TEST:
-    val theory = controller.getTheory(Path.parseM("https://example.com/tptpexptest?ExampleOfPLNaturalDeduction"))
+    val theory = controller.getTheory(Path.parseM("latin:/playground/tptp-exporter?Monoid"))
     // val translated = translate_theory(theory)
 
     // simulate context for: [F: prop, G: prop, pf: |- F /\ G] ...[BUTTON CLICK]...
     val ctx = Context(
       IncludeVarDecl(theory.path, args = Nil),
-      VarDecl(LocalName("F"), OMS(prop.path)),
-      VarDecl(LocalName("G"), OMS(prop.path)),
-      VarDecl(LocalName("pf"), ded(and(OMV("F"), OMV("G"))))
+      //VarDecl(LocalName("F"), OMS(prop.path)),
+      //VarDecl(LocalName("G"), OMS(prop.path)),
+      //VarDecl(LocalName("pf"), ded(and(OMV("F"), OMV("G"))))
     )
-    val what = ded(and(OMV("G"), OMV("F")))
-    //print(translate_theory(theory)(controller))
-    println(exportTPTP(ctx, List(what))(controller).pretty)
+    //val what = ded(and(OMV("G"), OMV("F")))
+    //print(new SFOLExporter().translate_theory(theory)(controller))
+    println(new SFOLExporter().exportTPTP(ctx, Nil)(controller).pretty)
   }
-
-  def exportTPTP(ctx: Context, what: List[Term])(implicit ctrl: Controller): Problem = {
-    // walk through ctx, collect all axioms
-    // especially, upon IncludeVarDecls, recurse into referenced theory
-    val axioms = ctx.mapVarDecls {
-      case (_, vd@IncludeVarDecl(_, _, _)) =>
-        val path = vd.tp.get.toMPath
-        translate_theory(controller.getTheory(path))
-      case (ctx, vd: VarDecl) =>
-        translate_var_decl(vd, ctx)(ctrl).toList
-    }.flatten.map(x => x.copy(role = "axiom"))
-
-   //for (a <- axioms) {
-   //  a.role = "axiom"
-   //}
-
-    val conjectures = what match {
-      case DedList(formulas) => formulas.map(f => FOFAnnotated("Conjecture", "conjecture",  FOF.Logical(translate_formula(f)), None))
-    }
-
-    Problem(List(), (axioms++conjectures))
-
-
-    //TPTPTheory("Theory", axioms, )
-
-
-  }
-
-
-  def translate_var_decl(vd: VarDecl, ctx: Context)(implicit ctrl: Controller): Option[FOFAnnotated] = {
-    val simplicationUnit = SimplificationUnit(ctx, expandDefinitions = true, fullRecursion = true)
-
-    val newTp = vd.tp.map(ctrl.simplifier(_, simplicationUnit))
-
-    newTp match {
-      case Some(ded(formula)) =>
-        Some(FOFAnnotated(vd.name.toString(), "", FOF.Logical(translate_formula(formula)), None))
-      case _ => None
-    }
-  }
-
-  def translate_constant(c: Constant)(implicit ctrl: Controller): Option[FOFAnnotated] = {
-    val ctx = Context(c.path.module)
-    val simplicationUnit = SimplificationUnit(ctx, expandDefinitions = true, fullRecursion = true)
-
-    val newTp = c.tp.map(ctrl.simplifier(_, simplicationUnit))
-
-    newTp match {
-      case Some(ded(formula)) =>
-        Some(FOFAnnotated(c.name.toString(), "", FOF.Logical(translate_formula(formula)), None))
-      case _ => None
-    }
-  }
-
-  def translate_theory(theory: Theory)(implicit ctrl: Controller): List[FOFAnnotated] = {
-    theory.getIncludesWithoutMeta.flatMap(include => translate_theory(ctrl.getTheory(include))) ++ theory.getConstants.flatMap(translate_constant)
-  }
-
-  def translate_formula(t: Term): FOF.Formula = t match {
-    case forall(Lambda(v, _, body)) =>
-      FOF.QuantifiedFormula(
-        FOF.!,
-        Seq(
-          v.toPath
-        ),
-        translate_formula(body)
-      )
-    case exists(Lambda(v, _, body)) =>
-      FOF.QuantifiedFormula(
-        FOF.?,
-        Seq(
-          v.toPath
-        ),
-        translate_formula(body)
-      )
-
-    case and(left, right) =>
-      FOF.BinaryFormula(FOF.&, translate_formula(left), translate_formula(right))
-    case or(left, right) =>
-      FOF.BinaryFormula(FOF.|, translate_formula(left), translate_formula(right))
-    case impl(left, right) =>
-      FOF.BinaryFormula(FOF.Impl, translate_formula(left), translate_formula(right))
-    case equiv(left, right) =>
-      FOF.BinaryFormula(FOF.<=>, translate_formula(left), translate_formula(right))
-    case not(arg) =>
-      FOF.UnaryFormula(FOF.~, translate_formula(arg))
-
-    case OMID(f) =>
-      FOF.AtomicFormula(f.name.toString, Nil)
-
-    case OMV(x) =>
-      FOF.AtomicFormula(x.toString, Nil)
-
-    case ApplySpine(OMID(f), args) =>
-      FOF.AtomicFormula(f.name.toString(), args.map(translate_term))
-
-  }
-
-  def translate_term(t: Term): FOF.Term = t match {
-    case ApplySpine(OMID(f), args) =>
-      // f: GlobalName, args: List[Term]
-      FOF.AtomicTerm(f.name.toString(), args.map(translate_term))
-
-    case OMID(f) =>
-      FOF.AtomicTerm(f.name.toString(), Nil)
-
-    //case OMV(x) =>
-    //  // x: LocalName
-    //  Var(x.name.toString())
-    case OMV(x) =>
-      // x: LocalName
-      FOF.Variable(x.toString())
-  }
-
 }
 
